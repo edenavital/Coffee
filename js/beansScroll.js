cf68d1552bda04798dab70274432685c4bfa6579
edenavital <edendor@gmail.com>
2019-09-08 23:38:05 +0300

    Loop on sections node list in order to add Event Listener efficiently

--- js/beansScroll.js
+++ js/beansScroll.js
@@ -1,22 +1,10 @@
 /*Optimize the sections, so instead of declaring each one of them, try to sections.forEach*/
-var sections = document.querySelectorAll("#table-scroll ul li a");
+const sections = document.querySelectorAll("#table-scroll ul li a");
 
-sections[0].addEventListener("click", function() {
-  smoothScroll("#section0", 1000);
-});
-
-sections[1].addEventListener("click", function() {
-  smoothScroll("#section1", 1000);
-});
-
-sections[2].addEventListener("click", function() {
-  smoothScroll("#section2", 1000);
-});
-
-sections[3].addEventListener("click", function() {
-  smoothScroll("#section3", 1000);
-});
-
-sections[4].addEventListener("click", function() {
-  smoothScroll("#section4", 1000);
-});
+for (let i = 0; i < sections.length; i++) {
+  (() => {
+    sections[i].addEventListener("click", () => {
+      smoothScroll("#section" + i, 1000);
+    });
+  })();
+}
